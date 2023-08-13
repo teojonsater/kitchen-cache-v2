f0371d88d9087e0ddf0e4833b74459a940928180
Teo J <36267770+teojonsater@users.noreply.github.com>
2023-08-13 22:34:55 +0200

    Initial react commit

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes";
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ client/src/Layout.tsx
@@ -0,0 +1,12 @@
+import {Outlet} from "react-router-dom";
+
+function Layout() {
+    return (
+        <>
+            <h1>Hej</h1>
+            <Outlet/>
+        </>
+    );
+}
+
+export default Layout;

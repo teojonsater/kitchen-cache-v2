3233aa90baa773193e3b15f1204bec9d07203819
AnnaSverrePanna <83768759+AnnaSverrePanna@users.noreply.github.com>
2023-08-16 23:11:18 +0200

    Added StatsCard

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,12 +1,14 @@
 import {Outlet} from "react-router-dom";
 import IngredientsCard from "./design/IngredientsCard";
 import StepsCard from "./design/StepsCard";
+import StatsCard from "./design/StatsCard";
 
 function Layout() {
     return (
         <>
             <IngredientsCard/>
             <StepsCard/>
+            <StatsCard/>
             <Outlet/>
         </>
     );

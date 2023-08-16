5d1ae2f5e997757c417c130dadb83f21c342468a
AnnaSverrePanna <83768759+AnnaSverrePanna@users.noreply.github.com>
2023-08-16 17:36:30 +0200

    Added a design in react for the steps card

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,10 +1,12 @@
 import {Outlet} from "react-router-dom";
 import IngredientsCard from "./design/IngredientsCard";
+import StepsCard from "./design/StepsCard";
 
 function Layout() {
     return (
         <>
             <IngredientsCard/>
+            <StepsCard/>
             <Outlet/>
         </>
     );

bbf4b6ce9c8ecc538392d37e6cb7ab0ef437d618
AnnaSverrePanna <83768759+AnnaSverrePanna@users.noreply.github.com>
2023-08-22 11:57:53 +0200

    Created non functional select

--- client/src/pages/AllRecipesPage/components/SearchBar/SearchBar.tsx
+++ client/src/pages/AllRecipesPage/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import type { ChangeEvent } from "react";
-import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import {
+	Input,
+	InputGroup,
+	InputLeftElement,
+	InputRightElement,
+	Select,
+} from "@chakra-ui/react";
 import FilterButton from "./FilterButton";
+import sortByOptions from "../../utils/sortByOptions";
 
 interface Props {
 	searchQuery: string;
@@ -11,12 +18,36 @@ interface Props {
 function SearchBar({ searchQuery, onSearchQueryChange }: Props) {
 	return (
 		<InputGroup>
+			<InputLeftElement
+				borderLeftRadius="full"
+				bgColor="teal.500"
+				w="fit-content"
+			>
+				<Select
+					borderLeftRadius="full"
+					focusBorderColor="teal.500"
+					color="white"
+				>
+					{sortByOptions.map((sortBy, index) => {
+						return (
+							<option
+								style={{ color: "black" }}
+								key={index}
+								value={sortBy}
+							>
+								{sortBy}
+							</option>
+						);
+					})}
+				</Select>
+			</InputLeftElement>
 			<Input
 				placeholder="Sök recept"
 				bgColor="white"
 				shadow="base"
 				borderRadius="full"
 				focusBorderColor="teal.500"
+				pl="36"
 				value={searchQuery}
 				onChange={onSearchQueryChange}
 			/>
